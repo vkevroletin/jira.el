b32686cb6ef5981b2bfc4b5ee150b61eee71d563
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-10-29 13:15:10 +1000

    Enable publishing coverage data to coverall.io

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,7 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
 (when (require 'undercover nil t)
-  (undercover "jiraffe.el" (:report-file "/tmp/local-report.json") (:send-report nil)))
+  (undercover "jiraffe.el"))
 
 (require 'ert)
 (require 'jiraffe)
